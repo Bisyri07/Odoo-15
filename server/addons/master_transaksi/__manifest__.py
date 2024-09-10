906c51c88fae3054185bac771e84f1e6d537e7de
Muhammad Bisyri Mustofa <mbisyri33@gmail.com>
2024-09-10 13:43:20 +0700

    Update list custom module & Create model (Table in the database)

--- server/addons/master_transaksi/__manifest__.py
+++ server/addons/master_transaksi/__manifest__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "master_transaksi",
+    'name': "Master Transaksi",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        This is Master data transaction module
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Bisyri",
+    'website': "https://github.com/Bisyri07/Odoo-15",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Transaksi',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -32,4 +32,7 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'application':True,
+    'installable': True,
+    'license': 'LGPL-3',
 }
---
+++ server/addons/master_transaksi/models/purchase_order.py
@@ -0,0 +1,36 @@
+from odoo import models, fields
+
+class PurchaseOrder(models.Model):  # inheritance
+    _name = "master_transaksi.purchase.order"
+    _description = "Purchase Order"
+
+    po_no = fields.Char(string='PO Number', required=True)
+    po_date = fields.Date(string='PO Date', required=True)
+    supplier_code = fields.Char(string='Supplier Code')
+    supplier = fields.Char(string='Supplier')
+    contact_person = fields.Char(string='Contact Person')
+    term_of_payment = fields.Char(string='Term of Payment')
+    expired_date = fields.Date(string='Expired Date')
+    currency = fields.Many2one('res.currency', string='Currency', required=True)  # Currency field
+    currency_rate = fields.Float(string='Currency Rate')
+    description = fields.Text(string='Description')
+    company_name = fields.Char(string='Company Name')
+    discount_pct = fields.Float(string='Discount (%)')
+    discount = fields.Monetary(string='Discount', currency_field='currency')  # Correct reference to currency
+    ppn_pct = fields.Float(string='PPN (%)')
+    ppn = fields.Monetary(string='PPN', currency_field='currency')
+    pph_pct = fields.Float(string='PPH (%)')
+    pph = fields.Monetary(string='PPH', currency_field='currency')
+    amount = fields.Monetary(string='Amount', currency_field='currency')
+    subtotal = fields.Monetary(string='Subtotal', currency_field='currency')
+    total = fields.Monetary(string='Total', currency_field='currency')
+    input_by = fields.Char(string='Input By')
+    date_input = fields.Datetime(string='Date Input', default=fields.Datetime.now)
+    posted_by = fields.Char(string='Posted By')
+    date_posted = fields.Datetime(string='Date Posted')
+    status = fields.Selection(
+        [('draft', 'Draft'), ('posted', 'Posted'), ('cancelled', 'Cancelled')], 
+        string='Status', default='draft'
+    )
+    # uom = fields.Char(string='UOM')
+    # po_type = fields.Char(string='PO Type')
